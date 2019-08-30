cb39e8d615433f60b54812d4e49e73ff1eabc1b7
mhsu <mhsu@mhsu-pc.wireless-1x.unc.edu>
2019-08-30 00:47:34 -0400

    Made the code reusable/adaptive towards any situation

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,71 +7,28 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		
 		// Your code follows here.
 		int totalCustomers = scan.nextInt();
-		//System.out.println(totalCustomers);
-		String nextLine = scan.next(); //1
-		char fr = nextLine.charAt(0);
-		String nextName = scan.next();//2
-		String ol = scan.next();//3
-		String a = scan.next();//4
-		String b = scan.next();//5
-		String c = scan.next();//6
-		String d = scan.next();//7
-		String e = scan.next();//8
-		String f = scan.next();//9
-		String g = scan.next();//10
-		String h = scan.next();//11
-		String i = scan.next();//12
-		String j = scan.next();//13
-		char frT = j.charAt(0);
-		String k = scan.next();//14
-		String l = scan.next();//15
-		
-		String m = scan.next();//16
-		String n = scan.next();//17
-		String o = scan.next();//18
-		String p = scan.next();//19
-		String q = scan.next();//20
-		String r = scan.next();//21
-		
-		String s = scan.next();//22
-		char frTT = s.charAt(0);
-		String t = scan.next();//23
-		String u = scan.next();//24
-		String v = scan.next();//25
-		String w = scan.next();//26
-		String x = scan.next();//27
-		
-		
-		Double first = Double.parseDouble(a);
-		Double second = Double.parseDouble(c);
-		Double third = Double.parseDouble(d);
-		Double fourth = Double.parseDouble(f);
-		Double fifth = Double.parseDouble(g);
-		Double sixth = Double.parseDouble(i);
+		for (int i = 0; i < totalCustomers; i++) {
+			Double grandTotal = 0.0;
 
-
-Double seventh = Double.parseDouble(m);
-Double eighth = Double.parseDouble(o);
-Double ninth = Double.parseDouble(p);
-Double tenth = Double.parseDouble(r);
-Double totals = ((first * second) + (third * fourth) + (fifth * sixth));
-
-Double totalst = ((seventh * eighth) + (ninth * tenth));
-
-Double eleventh = Double.parseDouble(v);
-Double twelth = Double.parseDouble(x);
-Double totalstt = ((eleventh * twelth) );
-scan.close();
-
-String newTotalstt = totalstt.toString();
-Float TotalFloat = Float.parseFloat(newTotalstt);
-System.out.println(fr + ". " + nextName+":" + " " + totals );
-System.out.println(frT + ". " + k+":" + " " + totalst );
-
-System.out.printf(frTT + ". " + t+":" + " " + "%.2f", TotalFloat);
-		
+			String nextLine = scan.next(); //1
+			char fr = nextLine.charAt(0);
+			String nextName = scan.next();//2
+			String ol = scan.next();//3
+			int groceries = Integer.parseInt(ol);
+			for (int j = 0; j < groceries; j++) {
+				String a = scan.next();//4
+				String b = scan.next();//5
+				String c = scan.next();//6
+				Double first = Double.parseDouble(a);
+				Double second = Double.parseDouble(c);
+				Double totals = first * second;
+				 grandTotal += totals;
+			}
+			String finalTotal = String.format("%.2f", grandTotal);
+			System.out.println(fr + ". " + nextName+":" + " " + finalTotal + " ");
+		}
 	}
 }
